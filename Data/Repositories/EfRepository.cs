d03eb88fdb285b3a2815a2504158f8d3e015c4bf
RoberthGo <robeg7183@gmail.com>
2026-02-18 22:07:12 -0600

    Corecciones de estructura

--- Data/Repositories/EfRepository.cs
+++
@@ -1,9 +0,0 @@
-// Entity Framework repository removed. Use the concrete SQL repositories in Data.Repositories (ServiceRepository, PartRepository) instead.
-// This file left as a marker and to avoid build errors if referenced elsewhere.
-namespace AutoRepair.Data.Repositories
-{
-    public class EfRepository<T> where T : class
-    {
-        // Intentionally left blank. Entity Framework support has been removed.
-    }
-}
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace AutoRepair
+﻿namespace ServicioAutomotriz
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,140 +23,17 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.leftPanel = new System.Windows.Forms.Panel();
-            this.btnServices = new System.Windows.Forms.Button();
-            this.btnParts = new System.Windows.Forms.Button();
-            this.mainPanel = new System.Windows.Forms.Panel();
-            this.toolPanel = new System.Windows.Forms.Panel();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.dataGrid = new System.Windows.Forms.DataGridView();
-            this.leftPanel.SuspendLayout();
-            this.mainPanel.SuspendLayout();
-            this.toolPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // leftPanel
-            // 
-            this.leftPanel.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.leftPanel.Controls.Add(this.btnParts);
-            this.leftPanel.Controls.Add(this.btnServices);
-            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.leftPanel.Location = new System.Drawing.Point(0, 0);
-            this.leftPanel.Name = "leftPanel";
-            this.leftPanel.Size = new System.Drawing.Size(200, 450);
-            this.leftPanel.TabIndex = 0;
-            // 
-            // btnServices
-            // 
-            this.btnServices.Location = new System.Drawing.Point(12, 20);
-            this.btnServices.Name = "btnServices";
-            this.btnServices.Size = new System.Drawing.Size(176, 40);
-            this.btnServices.TabIndex = 0;
-            this.btnServices.Text = "Servicios";
-            this.btnServices.UseVisualStyleBackColor = true;
-            this.btnServices.Click += new System.EventHandler(this.BtnServices_Click);
-            // 
-            // btnParts
-            // 
-            this.btnParts.Location = new System.Drawing.Point(12, 70);
-            this.btnParts.Name = "btnParts";
-            this.btnParts.Size = new System.Drawing.Size(176, 40);
-            this.btnParts.TabIndex = 1;
-            this.btnParts.Text = "Refacciones";
-            this.btnParts.UseVisualStyleBackColor = true;
-            this.btnParts.Click += new System.EventHandler(this.BtnParts_Click);
-            // 
-            // mainPanel
-            // 
-            this.mainPanel.Controls.Add(this.dataGrid);
-            this.mainPanel.Controls.Add(this.toolPanel);
-            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainPanel.Location = new System.Drawing.Point(200, 0);
-            this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(600, 450);
-            this.mainPanel.TabIndex = 1;
-            // 
-            // toolPanel
-            // 
-            this.toolPanel.Controls.Add(this.btnDelete);
-            this.toolPanel.Controls.Add(this.btnAdd);
-            this.toolPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.toolPanel.Location = new System.Drawing.Point(0, 0);
-            this.toolPanel.Name = "toolPanel";
-            this.toolPanel.Size = new System.Drawing.Size(600, 50);
-            this.toolPanel.TabIndex = 0;
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDelete.Location = new System.Drawing.Point(500, 10);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(80, 30);
-            this.btnDelete.TabIndex = 1;
-            this.btnDelete.Text = "Eliminar";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(12, 10);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(80, 30);
-            this.btnAdd.TabIndex = 0;
-            this.btnAdd.Text = "Agregar";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
-            // 
-            // dataGrid
-            // 
-            this.dataGrid.AllowUserToAddRows = true;
-            this.dataGrid.AllowUserToDeleteRows = true;
-            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGrid.Location = new System.Drawing.Point(0, 50);
-            this.dataGrid.Name = "dataGrid";
-            this.dataGrid.RowTemplate.Height = 24;
-            this.dataGrid.Size = new System.Drawing.Size(600, 400);
-            this.dataGrid.TabIndex = 1;
-            this.dataGrid.AutoGenerateColumns = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.mainPanel);
-            this.Controls.Add(this.leftPanel);
-            this.Name = "Form1";
-            this.Text = "AutoRepair";
-            this.leftPanel.ResumeLayout(false);
-            this.mainPanel.ResumeLayout(false);
-            this.toolPanel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
-            this.ResumeLayout(false);
-
+            this.Text = "Form1";
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel leftPanel;
-        private System.Windows.Forms.Button btnServices;
-        private System.Windows.Forms.Button btnParts;
-        private System.Windows.Forms.Panel mainPanel;
-        private System.Windows.Forms.DataGridView dataGrid;
-        private System.Windows.Forms.Panel toolPanel;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnDelete;
     }
 }
-
